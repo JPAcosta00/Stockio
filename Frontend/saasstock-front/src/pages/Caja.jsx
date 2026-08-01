b0136fed564d88db01a5ceae5ba1f5cad50478fd
Juan Pablo Acosta <Juampyacosta5@gmail.com>
2026-08-01 11:24:34 -0300

    Corrijo export de la funcion de caja

--- Frontend/saasstock-front/src/pages/Caja.jsx
+++ Frontend/saasstock-front/src/pages/Caja.jsx
@@ -1,2 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import apiClient from '../api/apiClient';
+
+export default function Caja() {
+  return (
+    <div>
+      {/* Tu componente */}
+      <h3>Aca iria todo el manejo de caja</h3>
+    </div>
+  );
+}
