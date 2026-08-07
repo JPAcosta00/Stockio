d522fb7152d122e19151ea6050535c08e9d7daf9
Juan Pablo Acosta <Juampyacosta5@gmail.com>
2026-08-07 00:39:58 -0300

    Arreglo error de import de alertas

--- Frontend/saasstock-front/src/routes/AppRoutes.jsx
+++ Frontend/saasstock-front/src/routes/AppRoutes.jsx
@@ -13,7 +13,6 @@ import Caja from '../pages/Caja';
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth(); 
 
-  //  Si todavía está leyendo el sessionStorage, muestra un estado de carga temporal
   if (loading) {
     return (
       <div className="min-h-screen bg-zinc-950 flex items-center justify-center text-zinc-400">
@@ -28,7 +27,6 @@ const PrivateRoute = ({ children }) => {
 const PublicRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth(); 
 
-  // Si está cargando, se espera
   if (loading) {
     return (
       <div className="min-h-screen bg-zinc-950 flex items-center justify-center text-zinc-400">
@@ -42,32 +40,34 @@ const PublicRoute = ({ children }) => {
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+    <AlertProvider>
+      <Routes>
+        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
 
-      {/* INICIO */}
-      <Route 
-        path="/" 
-        element={
-          <PrivateRoute>
-            <Estadisticas />
-          </PrivateRoute>
-        } 
-      />
+        {/* INICIO */}
+        <Route 
+          path="/" 
+          element={
+            <PrivateRoute>
+              <Estadisticas />
+            </PrivateRoute>
+          } 
+        />
 
-      {/* INVENTARIO REAL */}
-      <Route path="/caja" element={<PrivateRoute><Caja /></PrivateRoute>} />
+        {/* CAJA */}
+        <Route path="/caja" element={<PrivateRoute><Caja /></PrivateRoute>} />
 
-      {/* INVENTARIO REAL */}
-      <Route path="/inventario" element={<PrivateRoute><Inventario /></PrivateRoute>} />
+        {/* INVENTARIO REAL */}
+        <Route path="/inventario" element={<PrivateRoute><Inventario /></PrivateRoute>} />
 
-      {/* SECCION DE VENTTAS*/}
-      <Route path="/ventas" element={<PrivateRoute><Ventas /></PrivateRoute>} />
-      
-      {/* CONFIGURACIÓN (temporal) */}
-      <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+        {/* SECCION DE VENTAS */}
+        <Route path="/ventas" element={<PrivateRoute><Ventas /></PrivateRoute>} />
+        
+        {/* PERFIL */}
+        <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
 
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </AlertProvider>
   );
 }
